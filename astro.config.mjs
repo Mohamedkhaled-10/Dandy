1d453b39f1d89c68ee6f6573637254032a798e1f
NIGHTMAReS <midogames331@gmail.com>
2026-09-06 20:18:01 +0300

    fix 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,53 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-  redirects: {
-    '/pages/shop/all-products.html': '/all-products',
-    '/pages/shop/all-products': '/all-products',
-    '/pages/shop/special-offers.html': '/special-offers',
-    '/pages/shop/special-offers': '/special-offers',
-    '/pages/shop/product.html': '/product',
-    '/pages/shop/product': '/product',
-    '/pages/shop/cart.html': '/cart',
-    '/pages/shop/cart': '/cart',
-    '/pages/shop/invoice.html': '/invoice',
-    '/pages/shop/invoice': '/invoice',
-    '/pages/shop/track-order.html': '/track-order',
-    '/pages/shop/track-order': '/track-order',
-    '/pages/support/help.html': '/help',
-    '/pages/support/help': '/help',
-    '/pages/support/client-reviews.html': '/client-reviews',
-    '/pages/support/client-reviews': '/client-reviews',
-    '/pages/blog/blog.html': '/blog',
-    '/pages/blog/blog': '/blog',
-    '/pages/blog/post.html': '/post',
-    '/pages/blog/post': '/post',
-    '/pages/legal/about-us.html': '/about-us',
-    '/pages/legal/about-us': '/about-us',
-    '/pages/legal/contact-us.html': '/contact-us',
-    '/pages/legal/contact-us': '/contact-us',
-    '/pages/legal/privacy-policy.html': '/privacy-policy',
-    '/pages/legal/privacy-policy': '/privacy-policy',
-    '/pages/legal/terms.html': '/terms',
-    '/pages/legal/terms': '/terms',
-    '/pages/auth/login.html': '/login',
-    '/pages/auth/login': '/login',
-    '/pages/dashboard/dashboard.html': '/dashboard',
-    '/pages/dashboard/dashboard': '/dashboard',
-    '/pages/dashboard/dashboard-order.html': '/dashboard-order',
-    '/pages/dashboard/dashboard-order': '/dashboard-order',
-    '/pages/dashboard/dashboard-product.html': '/dashboard-product',
-    '/pages/dashboard/dashboard-product': '/dashboard-product',
-    '/pages/dashboard/dashboard-blog.html': '/dashboard-blog',
-    '/pages/dashboard/dashboard-blog': '/dashboard-blog',
-    '/pages/dashboard/dashboard-index.html': '/dashboard-index',
-    '/pages/dashboard/dashboard-index': '/dashboard-index',
-    '/pages/dashboard/statues.html': '/statues',
-    '/pages/dashboard/statues': '/statues'
+  output: 'static',
+  build: {
+    format: 'directory'
   }
 });
