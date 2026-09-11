3871c179a7add7b59591d05c220107658133a00a
NIGHTMAReS <midogames331@gmail.com>
2026-09-11 21:59:28 +0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import vercel from '@astrojs/vercel';
-
-const isVercel = Boolean(process.env.VERCEL || process.env.VERCEL_ENV);
 
 // https://astro.build/config
 export default defineConfig({
-  output: isVercel ? 'static' : 'server',
-  adapter: isVercel
-    ? vercel()
-    : node({
-        mode: 'standalone'
-      }),
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   build: {
     format: 'directory'
   }
