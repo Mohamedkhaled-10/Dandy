19f91e4a893e8432f96239c75fbe675d81855b33
NIGHTMAReS <midogames331@gmail.com>
2026-09-17 15:01:38 +0300

    seo production fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
+
+const isVercel = Boolean(process.env.VERCEL || process.env.VERCEL_ENV);
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: isVercel
+    ? vercel()
+    : node({
+        mode: 'standalone'
+      }),
   build: {
     format: 'directory'
   }
